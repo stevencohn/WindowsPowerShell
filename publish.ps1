719cb4048dd1dae7bafcce0cc93ce1743d688c89
Steven M. Cohn <steven_cohn@waters.com>
2018-03-21 02:50:52 -0400

    quiet

--- publish.ps1
+++ publish.ps1
@@ -4,4 +4,4 @@ $this = Split-Path -Leaf $PSCommandPath
 $spath = Split-Path -parent $PSCommandPath
 $tpath = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) "WindowsPowerShell"
 
-Copy-Item "$spath\*" "$tpath" -Recurse -Force -Exclude @("$this", ".git") -Verbose
+Copy-Item "$spath" "$tpath" -Recurse -Force -Exclude @("$this")
