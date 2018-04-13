780919ca325bb4af0333fd68376476ab8e9fc76f
Steven M. Cohn <steven_cohn@waters.com>
2018-04-12 22:29:21 -0400

    rgb values in get-consolecolors

--- Modules/Scripts/Get-ConsoleColors.ps1
+++ Modules/Scripts/Get-ConsoleColors.ps1
@@ -11,12 +11,13 @@ For ($i=0; $i -lt $colors.Length; $i++) `
 
 	$basics = "{0} {1} {2}" -f $table, $value.ToString('X8'), $colors[$i]
 	$decimal = "{0}, {1}, {2}" -f ($value -band 0xff), (($value -band 0xff00) -shr 8), (($value -band 0xff0000) -shr 16)
+	$rgb = "{0:X2}{1:X2}{2:X2}" -f ($value -band 0xff), (($value -band 0xff00) -shr 8), (($value -band 0xff0000) -shr 16)
 
 	if ($colors[$i] -eq 'Black') {
-		Write-Host ("{0,-35} {1}" -f $basics, $decimal) -ForegroundColor $colors[$i] -BackgroundColor DarkGray	
+		Write-Host ("{0,-35} #{1}  {2}" -f $basics, $rgb, $decimal) -ForegroundColor $colors[$i] -BackgroundColor DarkGray	
 	}
 	else {
-		Write-Host ("{0,-35} {1}" -f $basics, $decimal) -ForegroundColor $colors[$i]
+		Write-Host ("{0,-35} #{1}  {2}" -f $basics, $rgb, $decimal) -ForegroundColor $colors[$i]
 	}
 }
 
