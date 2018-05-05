c8e2cacdd044793da554d1ab69b8da1c265eb48f
Steven M. Cohn <steven_cohn@waters.com>
2018-05-05 12:01:11 -0400

    copy-console getting better

--- Modules/Scripts/Copy-Console.ps1
+++ Modules/Scripts/Copy-Console.ps1
@@ -19,22 +19,13 @@ param(
 	[parameter(Position = 0, Mandatory = $false)]
 	[string] $OutFile,	# output file name, default is clipboard
 
-	[switch] $All, # if true, get all lines in buffer
-	[switch] $Rtf, # capture as RTF, default is HTML
+	[switch] $All,		# if true, get all lines in buffer
+	[switch] $Rtf,		# capture as RTF, default is HTML
 	[switch] $Trim		# trim lines if true
 )
 
 Begin
 {
-	$font = 'Lucida Console'
-	$fontSize = '9pt'
-
-	$comap = @{} 	# color map
-
-	# character translations
-	$cmap = @{[char]'<' = '&lt;'; [char]'>' = '&gt;'; [char]'&' = '&amp;'}
-	#if ($Rtf) { $cmap = @{[char]"`t" = '\tab'; [char]'\' = '\\'; [char]'{' = '\{'; [char]'}' = '\}' } }
-
 	$colors = @{
 		'Black' = 0x1e1e1e
 		'DarkBlue' = 0x006291
@@ -49,10 +40,22 @@ Begin
 		'Green' = 0x00ff00
 		'Cyan' = 0x00ffff
 		'Red' = 0xff0000
-		'Magenta' = 0xff9158 #orange
+		'Magenta' = 0xff9158 # orange
 		'Yellow' = 0xffff00
 		'White' = 0xffffff
 	}
+
+	$font = 'Lucida Console'
+	$fontSize = '9pt'
+
+	# color map
+	$comap = @{}
+	$rawForeground = $host.UI.RawUI.ForegroundColor
+	$rawBackground = $host.UI.RawUI.BackgroundColor
+
+	# character translations
+	$cmap = @{[char]'<' = '&lt;'; [char]'>' = '&gt;'; [char]'&' = '&amp;'}
+	if ($Rtf) { $cmap = @{[char]"`t" = '\tab'; [char]'\' = '\\'; [char]'{' = '\{'; [char]'}' = '\}' } }
 
 	function MakeColorMap ()
 	{
@@ -61,43 +64,25 @@ Begin
 			$map = New-Object System.Text.StringBuilder
 			#{\colortbl;red0\green0\blue128;\red0\green128\blue0;
 			$null = $map.Append('{\colortbl;')
-			foreach ($color in $colors)
+			foreach ($color in $colors.Keys)
 			{
 				$rgb = $colors[$color]
 				$null = $map.Append('\red' + (($rgb -band 0xFF0000) -shr 16))
 				$null = $map.Append('\green' + (($rgb -band 0xFF00) -shr 8))
-				$null = $map.Append('\blue' + ($rgb -band 0xFF))
+				$null = $map.Append('\blue' + ($rgb -band 0xFF) + ';')
 			}
 			$map = $map.Append('}').ToString()
 		}
 		else
 		{
-			$comap = @{}
-			foreach ($color in $colors)
+			$map = @{}
+			foreach ($color in $colors.Keys)
 			{
-				$comap.Add($color, '#' + $colors[$color].ToString('X6'))
+				$map.Add($color, '#' + $colors[$color].ToString('X6'))
 			}
 		}
-	}
 
-	# console colour mapping
-	$comap = @{
-		'Black' = '#1e1e1e'
-		'DarkBlue' = '#006291'
-		'DarkGreen' = '#008000'
-		'DarkCyan' = '#008080'
-		'DarkRed' = '#800000'
-		'DarkMagenta' = '#800080'
-		'DarkYellow' = '#808000'
-		'Gray' = '#dedede'
-		'DarkGray' = '#808080'
-		'Blue' = '#178bff'
-		'Green' = '#00ff00'
-		'Cyan' = '#00ffff'
-		'Red' = '#ff0000'
-		'Magenta' = '#ff9158' #orange
-		'Yellow' = '#ffff00'
-		'White' = '#ffffff'
+		$map
 	}
 
 	function GetDimensions ()
@@ -129,39 +114,28 @@ Begin
 	# this is must faster than trying to print out all of that whitespace!
 	function GetBoundaries ($cells, $width, $height)
 	{
-		$top = 0
-		$found = $false
-		for ([int]$r = 0; $r -lt $height -and !$found; $r++)
-		{
-			for ([int]$c = 0; $c -lt $width -and !$found; $c++)
-			{
+		$top = -1
+		for ([int]$r = 0; $r -lt $height -and $top -lt 0; $r++) {
+			for ([int]$c = 0; $c -lt $width -and $top -lt 0; $c++) {
 				$ch = $cells[$r, $c].Character
-				$found = -not [String]::IsNullOrWhiteSpace($ch)
-				$top = $r
+				if (![String]::IsNullOrWhiteSpace($ch)) { $top = $r }
 			}
 		}
 
-		if ($found)
-		{
-			$bottom = $height
-			$found = $false
-			for ([int]$r = $height; $r -gt 0 -and !$found; $r--)
-			{
-				for ([int]$c = 0; $c -lt $width -and !$found; $c++)
-				{
-					$ch = $cells[$r, $c].Character
-					$found = -not [String]::IsNullOrWhiteSpace($ch)
-					$bottom = $r
-				}
-			}
+		if ($top -lt 0) { return 0, $height }
 
-			return $top, $bottom
+		$bottom = -1
+		for ([int]$r = $height; $r -gt 0 -and $bottom -lt 0; $r--) {
+			for ([int]$c = 0; $c -lt $width -and $bottom -lt 0; $c++) {
+				$ch = $cells[$r, $c].Character
+				if (![String]::IsNullOrWhiteSpace($ch)) { $bottom = $r }
+			}
 		}
 
-		return 0, $height
+		return $top, $bottom
 	}
 
-	function WritePreamble ($builder, $fg, $bg)
+	function WritePreamble ($builder, [string]$fg, [string]$bg)
 	{
 		if ($Rtf)
 		{
@@ -169,40 +143,81 @@ Begin
 			$null = $builder.Append("{\rtf1\fbidis\ansi\ansicpg1252\deff0\deflang1033{\fonttbl{\f0\fnil\fcharset0 $font;}}")
 			$null = $builder.Append("`r`n")
 			# Append RTF color table which will contain all Powershell console colors.
-			$null = $builder.Append('{\colortbl;red0\green0\blue128;\red0\green128\blue0;\red0\green128\blue128;\red128\green0\blue0;\red1\green36\blue86;\red238\green237\blue240;\red192\green192\blue192;\red128\green128\blue128;\red0\green0\blue255;\red0\green255\blue0;\red0\green255\blue255;\red255\green0\blue0;\red255\green0\blue255;\red255\green255\blue0;\red255\green255\blue255;\red0\green0\blue0;}')
+			$null = $builder.Append($comap)
 			$null = $builder.Append("`r`n")
 			# Append RTF document settings.
 			$null = $builder.Append('\viewkind4\uc1\pard\ltrpar\f0\fs23 ')
 		}
 		else
 		{
-			$null = $builder.Append("<pre style='color:$(col2htm $fg); background-color:$(col2htm $bg); font-family:$font; font-size:$fontSize; margin:0 10pt 0 0; line-height:normal;'>")
+			$null = $builder.Append("<pre style='color:$($comap[$fg]); background-color:$($comap[$bg]); font-family:$font; font-size:$fontSize; margin:0 10pt 0 0; line-height:normal;'>")
 		}
+
+		$null = $builder.Append([Environment]::NewLine)
 	}
+
+	function StartColor ($builder, [string]$fg, [string]$bg)
+	{
+		if ($Rtf)
+		{
+			$index = [Array]::IndexOf($colors.Keys, $fg)
+			$null = $builder.Append("{\cf$index")
+			# You can also add \ab* tag here if you want a bold font in the output.
+			$index = [Array]::IndexOf($colors.Keys, $bg)
+			$null = $builder.Append("\chshdng0\chcbpat$index")
+		}
+		else
+		{
+			$fc = $comap[$fg]
+			$bc = $comap[$bg]
+			$null = $line.Append("<span style='color:$fc; background-color:$bc'>")
+		}
+
+		$fg, $bg
+	}
+
+	function EndColor ($builder)
+	{
+		if ($Rtf)
+		{
+			$null = $builder.Append('}')
+		}
+		else
+		{
+			$null = $builder.Append('</span>')
+		}
+
+		$rawForeground, $rawBackground
+	}
+
 
 	function WritePostscript ($builder)
 	{
-		if (!$Rtf)
+		if ($Rtf)
+		{
+			$null = $builder.Append('}')
+		}
+		else
 		{
 			$null = $builder.Append('</pre>')
 		}
 	}
 
-	function col2htm { $comap[[string]$args[0]] }
-
 	function ch2htm { if ($cmap[[char]$args[0]]) { $cmap[[char]$args[0]] } else { $args[0] } }
 
 	function CopyToClipboard ([string] $buffer)
 	{
-		$null = [Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-		$dataObject = New-Object Windows.Forms.DataObject 
-
-		#$dataObject.SetData([Windows.Forms.DataFormats]::UnicodeText, $true, $buffer)
-		$builder.ToString() | Set-Clipboard -AsHtml
-		#$dataObject.SetData([Windows.Forms.DataFormats]::RTF, $true, $rtf)
-		#$dataObject.SetData([Windows.Forms.DataFormats]::HTML, $true, $buffer)
-
-		[Windows.Forms.Clipboard]::SetDataObject($dataObject, $true)
+		if ($Rtf)
+		{
+			$null = [Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+			$clip = New-Object Windows.Forms.DataObject 
+			$clip.SetText($buffer, [Windows.Forms.TextDataFormat]::Rtf)
+			[Windows.Forms.Clipboard]::SetDataObject($clip, $true)
+		}
+		else
+		{
+			$buffer | Set-Clipboard -AsHtml
+		}
 	}
 }
 Process
@@ -223,38 +238,33 @@ Process
 	$top, $bottom = GetBoundaries $cells $width $height
 	Write-Verbose("boundaries top {0} bottom {1}" -f $top, $bottom)
 
-	$defaultfg = $host.UI.RawUI.ForegroundColor
-	$defaultbg = $host.UI.RawUI.BackgroundColor
-	$fg = $defaultfg
-	$bg = $defaultbg
+	$comap = MakeColorMap
 
 	$builder = New-Object System.Text.StringBuilder
-	WritePreamble $builder $fg $bg
 
-	for ([int]$row = $top; $row -lt $bottom; $row++)
+	$foreground = $rawForeground
+	$background = $rawBackground
+
+	WritePreamble $builder $foreground $background
+
+	for ($r = $top; $r -lt $bottom; $r++)
 	{
 		$line = New-Object System.Text.StringBuilder
-		for ([int]$col = 0; $col -lt $width; $col++)
+		for ($c = 0; $c -lt $width; $c++)
 		{
-			$cell = $cells[$row, $col]
-			# do we need to change colours?
-			$cfg = [string]$cell.ForegroundColor
-			$cbg = [string]$cell.BackgroundColor
-			if ($fg -ne $cfg -or $bg -ne $cbg)
+			$cell = $cells[$r, $c]
+			if ($foreground -ne $cell.ForegroundColor -or $background -ne $cell.BackgroundColor)
 			{
-				if ($fg -ne $defaultfg -or $bg -ne $defaultbg)
+				if ($foreground -ne $rawForeground -or $background -ne $rawBackground)
 				{
-					$null = $line.Append('</span>') # remove any specialisation
-					$fg = $defaultfg; $bg = $defaultbg;
+					$foreground, $background = EndColor $line
 				}
-				if ($cfg -ne $defaultfg -or $cbg -ne $defaultbg)
+				if ($cell.ForegroundColor -ne $rawForeground -or $cell.BackgroundColor -ne $rawBackground)
 				{
-					# start a new colour span
-					$null = $line.Append("<span style='color: $(col2htm $cfg); background-color: $(col2htm $cbg)'>")
+					$foreground, $background = StartColor $line $cell.ForegroundColor $cell.BackgroundColor
 				}
-				$fg = $cfg
-				$bg = $cbg
 			}
+
 			$ch = ch2htm $cell.Character
 			$null = $line.Append($ch)
 		}
@@ -283,6 +293,6 @@ Process
 	}
 	else
 	{
-		$builder.ToString() | Out-File $OutFile
+		$builder.ToString() | Out-File $OutFile -Encoding ASCII
 	}
 }
