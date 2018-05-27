81b361a995057f82b301fa1f77ea39ea8cf88dfb
Steven M. Cohn <steven_cohn@waters.com>
2018-05-27 11:56:56 -0400

    Get-Colors Set-Color for Cmd, PowerShell, and ConEmu

---
+++ Modules/Scripts/Get-Colors.ps1
@@ -0,0 +1,201 @@
+<#
+.SYNOPSIS
+Display the console colors. Default is to show the color table for the PowerShell
+console only.
+
+.PARAMETER All
+Display colors for all consoles.
+
+.PARAMETER Cmd
+Display colors for the Windows Command console only.
+
+.PARAMETER ConEmu
+Display colors for the ConEmu Command console only.
+
+.PARAMETER PS
+Display colors for the PowerShell console only.
+
+.DESCRIPTION
+Registry values are stored as B-G-R DWord values. This command displays the BGR
+value as well as the RGB, decimal equivalents, and console Esc sequences.
+#>
+
+param (
+	[switch] $All,
+	[Switch] $Cmd,
+	[Switch] $PS,
+	[Switch] $ConEmu)
+
+Begin
+{
+	# unicode box characters
+	$topleft = ([char]0x250C).ToString()
+	$topright = ([char]0x2510).ToString()
+	$bottomleft = ([char]0x2514).ToString()
+	$bottomright  = ([char]0x2518).ToString()
+	$horizontal = ([char]0x2500).ToString()
+	$vertical = ([char]0x2502).ToString()
+
+	$esc = [char]27
+	$colors = [System.Enum]::GetNames([System.ConsoleColor])
+
+	function Box ($text)
+	{
+		$max = 85
+		#$max = 0
+		#foreach ($s in $text) { if ($s.Length -gt $max) { $max = $s.Length } }
+
+		Write-Host
+		Write-Host($topleft + ($horizontal * ($max + 2)) + $topright)
+
+		foreach ($s in $text) {
+			Write-Host($vertical + ' ' + ("{0,-$max}" -f $s) + ' ' + $vertical)
+		}
+
+		Write-Host($bottomleft + ($horizontal * ($max + 2)) + $bottomright)
+		Write-Host
+	}
+
+	function ShowCommandColors ()
+	{
+		$codes = @{
+			'Black'       = @{ 'Fore'=30; 'Back'=40 }
+			'DarkRed'     = @{ 'Fore'=31; 'Back'=41 }
+			'DarkGreen'   = @{ 'Fore'=32; 'Back'=42 }
+			'DarkYellow'  = @{ 'Fore'=33; 'Back'=43 }
+			'DarkBlue'    = @{ 'Fore'=34; 'Back'=44 }
+			'DarkMagenta' = @{ 'Fore'=35; 'Back'=45 }
+			'DarkCyan'    = @{ 'Fore'=36; 'Back'=46 }
+			'Gray'        = @{ 'Fore'=37; 'Back'=47 }
+			'DarkGray'    = @{ 'Fore'=90; 'Back'=100 }
+			'Red'         = @{ 'Fore'=91; 'Back'=101 }
+			'Green'       = @{ 'Fore'=92; 'Back'=102 }
+			'Yellow'      = @{ 'Fore'=93; 'Back'=103 }
+			'Blue'        = @{ 'Fore'=94; 'Back'=104 }
+			'Magenta'     = @{ 'Fore'=95; 'Back'=105 }
+			'Cyan'        = @{ 'Fore'=96; 'Back'=106 }
+			'White'       = @{ 'Fore'=97; 'Back'=107 }
+		}
+
+		Box @(
+			'Command Console Colors',
+			'  System.ConsoleColor :: HKCU:\Console :: -ForegroundColor')
+
+		Write-Host 'Name         BGR      ConsoleColor  RGB      Decimal         Escape Sequence'
+
+		Push-Location HKCU:\Console
+
+		For ($i=0; $i -lt $colors.Length; $i++)
+		{
+			$name = $colors[$i]
+			$table = ("ColorTable{0}" -f ($i).ToString('00'))
+			$value = (Get-ItemPropertyValue . -name $table)
+
+			$basics = "{0} {1} {2}" -f $table, $value.ToString('X8'), $name
+			$decimal = "{0}, {1}, {2}" -f ($value -band 0xff), (($value -band 0xff00) -shr 8), (($value -band 0xff0000) -shr 16)
+			$rgb = "{0:X2}{1:X2}{2:X2}" -f ($value -band 0xff), (($value -band 0xff00) -shr 8), (($value -band 0xff0000) -shr 16)
+			$escapes = "<ESC>[{0}m" -f $codes[$name].Fore
+
+			if ($name -eq 'Black') {
+				Write-Host ("{0,-35} #{1}  {2,-14}  {3}  " -f $basics, $rgb, $decimal, $escapes) -ForegroundColor $name -BackgroundColor DarkGray	-NoNewline
+				Write-Host (" <ESC>[{0} " -f $codes[$name].Back) -BackgroundColor $name -ForegroundColor Gray
+			}
+			else {
+				Write-Host ("{0,-35} #{1}  {2,-14}  {3}  " -f $basics, $rgb, $decimal, $escapes) -ForegroundColor $name -NoNewline
+				$fore = if ($name -eq 'Gray') { 'DarkGray' } else { 'Gray' }
+				Write-Host (" <ESC>[{0}m " -f $codes[$name].Back) -BackgroundColor $name -ForegroundColor $fore
+			}
+		}
+
+		Write-Host "`nOther ESCapes: " -ForegroundColor White -NoNewline
+		Write-Host "<ESC>[0m Reset, $esc[1m<ESC>[1m Bold$esc[0m, $esc[4m<ESC>[4m Underline$esc[0m, $esc[7m<ESC>[7m Inverse$esc[0m`n" -ForegroundColor DarkGray
+
+		Pop-Location
+	}
+
+	function ShowPowerShellColors ()
+	{
+		Box @(
+			'PowerShell Console Colors',
+			'  $env:APPDATA +',
+			'    .\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk')
+
+		$menupath = "${env:APPDATA}\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk"
+
+		Write-Host 'Known Name            ConsoleColor  RGB      Decimal'
+
+		$link = Get-Link -Path $menupath
+
+		For ($i=0; $i -lt $colors.Length; $i++)
+		{
+			$name = $colors[$i]
+			$lolor = $link.ConsoleColors[$i]
+			$rgb = "{0:X2}{1:X2}{2:X2}" -f $lolor.R, $lolor.G, $lolor.B
+			$decimal = "{0}, {1}, {2}" -f $lolor.R, $lolor.G, $lolor.B
+
+			if ($name -eq 'Black') {
+				Write-Host ("{0,-21} {1,-13} #{2}  {3,-14}" -f $lolor.Name, $name, $rgb, $decimal) -ForegroundColor $name -BackgroundColor DarkGray
+			}
+			else {
+				Write-Host ("{0,-21} {1,-13} #{2}  {3,-14}" -f $lolor.Name, $name, $rgb, $decimal) -ForegroundColor $name
+			}
+		}
+	}
+
+	function ShowConEmuColors ()
+	{
+		$file = "${env:APPDATA}\ConEmu.xml"
+		if (Test-Path $file)
+		{
+			Box @('ConEmu Color Table', "  $file")
+			Write-Host 'Name         BGR      ConsoleColor  RGB      Decimal'
+
+			$xml = [xml](Get-Content $file) | Select-Xml `
+				-XPath "//key[@name='Software']/key[@name='ConEmu']/key[@name='.Vanilla']/value" | `
+				? { $_.Node.Name -like 'ColorTable*' } | % `
+				{
+					$index = [System.Int32]::Parse($_.Node.Name.Substring(10))
+					if ($index -lt 16)
+					{
+						$name = $colors[$index]
+						$bgr = [System.Convert]::ToInt32($_.Node.Data, 16)
+						$rgb = (($bgr -band 0xFF0000) -shr 16) + ($bgr -band 0x00FF00) + (($bgr -band 0x0000FF) -shl 16)
+						$basics = "{0} {1} {2}" -f $_.Node.Name, $bgr.ToString('X8'), $name
+						$decimal = "{0}, {1}, {2}" -f ($bgr -band 0xff), (($bgr -band 0xff00) -shr 8), (($bgr -band 0xff0000) -shr 16)
+
+						if ($name -eq 'Black') {
+							Write-Host ("{0,-35} #{1:X6}  {2,-14}" -f $basics, $rgb, $decimal) -ForegroundColor $name -BackgroundColor DarkGray
+						}
+						else {
+							Write-Host ("{0,-35} #{1:X6}  {2,-14}" -f $basics, $rgb, $decimal) -ForegroundColor $name
+						}
+					}
+				}
+		}
+	}
+}
+Process
+{
+	if ($Cmd)
+	{
+		ShowCommandColors
+	}
+	elseif ($ConEmu)
+	{
+		ShowConEmuColors
+	}
+	elseif ($PS)
+	{
+		ShowPowerShellColors
+	}
+	elseif ($All)
+	{
+		ShowPowerShellColors
+		ShowCommandColors
+		ShowConEmuColors
+	}
+	else
+	{
+		ShowPowerShellColors
+	}
+}
---
+++ Modules/Scripts/Set-Color.ps1
@@ -0,0 +1,213 @@
+<#
+.SYNOPSIS
+Set a custom value for the specified console color table entry.
+By default, the all color tables - Command, PowerShell, and ConEmu - are
+updated unless one table is specified.
+
+.PARAMETER Name
+The name of the color table entry to set. Must be one of the known
+System.ConsoleColor enumeration names.
+
+.PARAMETER Color
+The RGB or BGR color expressed as a six digit hex value.
+The default is RGB unless the -Bgr switch is specified.
+
+.PARAMETER Bgr
+Indicates that the Color parameter specifies a BGR value; 
+the default is to specify an RGB value.
+
+.PARAMETER Cmd
+Update just the Command console color table.
+
+.PARAMETER ConEmu
+Update just the ConEmu console color table.
+
+.PARAMETER PS
+Update just the PowerShell console color table.
+
+.PARAMETER WhatIf
+Run the command and report changes but don't make any changes.
+
+.DESCRIPTION
+Colors for PowerShell and command windows are stored separately. Command
+console colors are stored in the Registry while PowerShell colors are
+stored in the shortcut link file used to start the PowerShell console.
+#>
+
+# CmdletBinding adds -Verbose functionality, SupportsShouldProcess adds -WhatIf
+[CmdletBinding(SupportsShouldProcess=$true)]
+
+param (
+	[parameter(Position=0, Mandatory=$true, HelpMessage="First parameter is table entry name")]
+	[ValidateScript({
+		if ([bool]($_ -as [System.ConsoleColor] -is [System.ConsoleColor])) { $true } else {
+			Throw 'Name must specify a known System.ConsoleColor name'
+		}
+	})]
+	[string] $Name,
+
+	[parameter(Position=1, Mandatory=$true, HelpMessage="Second parameter is the color hex value")]
+	[ValidateScript({
+		if ($_ -match '^(0x|#)?[A-Fa-f0-9]{1,6}$') { $true } else {
+			Throw 'Value must specify a color hex value of 1-6 characters '
+		}
+	})]
+	[string] $Color,
+	[switch] $Bgr,
+	[switch] $Cmd,
+	[switch] $ConEmu,
+	[switch] $PS
+)
+
+Begin
+{
+	function GetColorIndex ($name)
+	{
+		# correct for case-sensitivity, find index of name within color table
+		return [System.Enum]::GetNames([System.ConsoleColor]).indexOf(($name -as [System.ConsoleColor]).ToString())
+	}
+
+	function NormalizeColor ($color)
+	{
+		if ($color.StartsWith('0x', [System.StringComparison]::InvariantCultureIgnoreCase))
+		{
+			$color = $color.Substring(2)
+		}
+		elseif ($color.StartsWith('#'))
+		{
+			$color = $color.Substring(1)
+		}
+
+		return [System.Convert]::ToInt32($color, 16)
+	}
+
+	function Reverse ($value)
+	{
+		# RGB to BGR, or BGR to RGB
+		return (($value -band 0xFF0000) -shr 16) + ($value -band 0x00FF00) + (($value -band 0x0000FF) -shl 16)
+	}
+
+	function SetCommandColor ()
+	{
+		$index = GetColorIndex $Name
+		$entry = ("ColorTable{0:00}" -f $index)
+
+		$value = NormalizeColor $Color
+		if (-not $Bgr) { $value = Reverse $value }
+
+		if ($WhatIfPreference)
+		{
+			$hex = ("{0:X6}" -f $value).ToUpper()
+			Write-Host "CMD: Set-ItemProperty HKCU:\Console -Name $entry -Value $value (#$hex) -Type DWord" -ForegroundColor DarkGray
+		}
+		else
+		{
+			Write-Host 'Setting command console color'
+			Push-Location HKCU:\Console
+			Set-ItemProperty . -Name $entry -Value $value -Type DWord
+			Pop-Location
+		}
+	}
+
+	function SetPowerShellColor ()
+	{
+		$folder = "${env:APPDATA}\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\"
+
+		$index = GetColorIndex $Name
+
+		$value = NormalizeColor $Color
+		if ($Bgr) { $value = Reverse $value }
+
+		$rgb = '#' + ("{0:X6}" -f $value).ToUpper()
+
+		# 64-bit shortcut
+		$linkpath = "$folder\Windows PowerShell.lnk"
+		if (Test-Path $linkpath)
+		{
+			$link = Get-Link -Path $linkpath
+			$link.ConsoleColors[$index] = $rgb
+
+			#$lnk.PopUpBackgroundColor=0x...
+			#$lnk.PopUpTextColor=0x...
+			#$lnk.ScreenBackgroundColor=0x...
+			#$lnk.ScreenTextColor=0x...
+
+			if ($WhatIfPreference) {
+				Write-Host "PWS: Update x64 LNK -Name $Name -Index $index -Value $rgb" -ForegroundColor DarkGray
+			}
+			else {
+				Write-Host 'Setting PowerShell console color'
+				$link.Save()
+			}
+		}
+
+		# 32-bit shortcut
+		$linkpath = "$folder\Windows PowerShell (x86).lnk"
+		if (Test-Path $linkpath)
+		{
+			$link = Get-Link -Path $linkpath
+			$link.ConsoleColors[$index] = $rgb
+
+			if ($WhatIfPreference) {
+				Write-Host "PWS: Update x86 LNK -Name $Name -Index $index -Value $rgb" -ForegroundColor DarkGray
+			}
+			else {
+				Write-Host 'Setting PowerShell x86 console color'
+				$link.Save()
+			}
+		}
+	}
+
+	function SetConEmuColor ()
+	{
+		$file = "${env:APPDATA}\ConEmu.xml"
+		if (Test-Path $file)
+		{
+			$index = GetColorIndex $Name
+			$entry = ("ColorTable{0:00}" -f $index)
+
+			$value = NormalizeColor $Color	
+			if (-not $Bgr) { $value = Reverse $value }
+
+			$value = ("{0:X8}" -f $value).ToUpper()
+
+			$xml = [xml](Get-Content $file)
+			$xml | Select-Xml -XPath "//value[@name='$entry']" | % `
+			{
+				$_.Node.data = $value
+			}
+
+			if ($WhatIfPreference) {
+				Write-Host "EMU: Set attribute $entry to $value" -ForegroundColor DarkGray
+			}
+			else {
+				Write-Host 'Setting ConEmu console color'
+				# convert XmlDocument to XElement to output formatted XML better
+				$null = [Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq")
+				$xml = [System.Xml.Linq.XElement]::Parse($xml.OuterXml)
+				$xml.Save($file)
+			}
+		}
+	}
+}
+Process
+{
+	if ($Cmd)
+	{
+		SetCommandColor
+	}
+	elseif ($ConEmu)
+	{
+		SetConEmuColor
+	}
+	elseif ($PS)
+	{
+		SetPowerShellColor
+	}
+	else
+	{
+		SetCommandColor
+		SetConEmuColor
+		SetPowerShellColor
+	}
+}
