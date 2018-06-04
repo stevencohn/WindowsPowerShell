8530034b0495c5ed22dea25ee173344caad711eb
Steven Cohn <steven_cohn@waters.com>
2018-06-04 15:16:56 -0400

    robocopy for remove-locked

--- Modules/Scripts/Remove-Locked.ps1
+++ Modules/Scripts/Remove-Locked.ps1
@@ -1,9 +1,17 @@
 <#
-.synopsis
-Remove a System-owned file or directory.
+.SYNOPSIS
+Remove a System-owned file or directory or a directory containing
+a path that is too long.
 
-.parameter name
+.PARAMETER name
 The path of the file or directory to remove.
+
+.DESCRIPTION
+For file and directories, the first attempt is to take ownership
+of all objects, change ACL (access control lists) and then delete.
+
+Additionally, for directories, will use "robocopy empty" method 
+of overwriting directory containing gratuitously long paths.
 #>
 
 param ([string] $name)
@@ -13,11 +21,26 @@ if (!(Confirm-Elevated (Split-Path -Leaf $PSCommandPath) -warn)) { return }
 
 if ((Get-Item $name) -is [System.IO.DirectoryInfo])
 {
-	# /F<name>==path-to-object /R==recurse /A==grant-admins /D:Y=default-prompt-answer
-	takeown /F $name /R /A /D Y
-	# /T==recurse /Q==supress-messages
-	icacls $name /grant administrators:f /t /q
-	Remove-Item $name -Force -Confirm:$false -Recurse
+	try
+	{
+		# /F<name>==path-to-object /R==recurse /A==grant-admins /D:Y=default-prompt-answer
+		takeown /F $name /R /A /D Y
+		# /T==recurse /Q==supress-messages
+		icacls $name /grant administrators:f /t /q
+		Remove-Item $name -Force -Confirm:$false -Recurse
+	}
+	catch
+	{
+		$empty = "${env:TEMP}\emptydir"
+		New-Item $empty -Force -Confirm:$false
+		robocopy $empty $name /purge
+		Remove-Item $empty -Force -Confirm:$false
+
+		if (Test-Path $name)
+		{
+			Remove-Item $name -Force -Confirm:$false
+		}
+	}
 }
 else
 {
