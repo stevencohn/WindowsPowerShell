1349ffc3118bf4f6d5768df9ee6601f84361cec9
Steven M. Cohn <steven_cohn@waters.com>
2018-06-12 08:00:43 -0400

    Test-RebootPending

--- Modules/Scripts/Test-RebootPending.ps1
+++ Modules/Scripts/Test-RebootPending.ps1
@@ -1,18 +1,80 @@
-#Adapted from https://gist.github.com/altrive/5329377
-#Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
+<#
+.SYNOPSIS
+Check the pending reboot status of the local computer.
 
-if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
-if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
-if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
-try
-{ 
-	$util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-	$status = $util.DetermineIfRebootPending()
-	if (($status -ne $null) -and $status.RebootPending)
+.PARAMETER Report
+If specified then return a report object of individual properties,
+default is to return a Boolean whether a reboot is pending.
+
+.OUTPUTS
+By default, return a Boolean indicating whether a reboot is pending.
+If -Report is specified then return an object describing all reboot related
+properties.
+
+.LINK
+https://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542
+#>
+
+param ([switch] $Report)
+
+Begin
+{
+	function GetStatusReport ()
 	{
-		return $true
+		# Trusted Installer related
+		if ($host.Version.Build -ge 6001)
+		{
+			$0 = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending'
+			$cbs = ((Get-ChildItem $0 -ErrorAction SilentlyContinue) -ne $null)
+		}
+		else
+		{
+			$cbs = $false
+		}
+
+		# Windows update related
+		$0 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired'
+		$wua = ((Get-Item $0 -ErrorAction SilentlyContinue) -ne $null)
+
+		try
+		{ 
+			$util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+			$status = $util.DetermineIfRebootPending()
+			$sdk = [bool](($status -ne $null) -and $status.RebootPending)
+		}
+		catch
+		{
+			$sdk = $false
+		}
+
+		# MSI file rename related
+		$0 = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager'
+		$msi = ((Get-ItemPropertyValue $0 -Name PendingFileRenameOperations -ErrorAction SilentlyContinue) | `
+			? { $_ -notmatch 'TEMP' -and $_ -ne '' }).Count -gt 0
+
+		## computer rename related
+		$currName = (Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' -Name 'ComputerName')
+		$pendName = (Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' -Name 'ComputerName')
+		$com = ($currName -ne $pendName)
+
+		return @{
+			CBSServicing = $cbs
+			MSIFileRename = $msi
+			WindowsUpdate = $wua
+			ClientSDK = $sdk
+			ComputerRename = $com
+		}
 	}
 }
-catch {}
- 
-return $false
+Process
+{
+	$status = GetStatusReport
+
+	if ($Report)
+	{
+		return $status
+	}
+
+	return $status.CBSServicing -or $status.MSIInstaller -or $status.WindowsUpdate `
+		-or $status.ClientSDK -or $status.FileRename -or $status.ComputerRename
+}
