6a7c85f0f3eadf8d85294a4b533325bc3d136352
Steven Cohn <steven_cohn@waters.com>
2018-06-22 13:27:18 -0400

    Show-Docker colorized

--- Modules/Scripts/Show-Docker.ps1
+++ Modules/Scripts/Show-Docker.ps1
@@ -1,15 +1,51 @@
 
 <#
 .SYNOPSIS
-Show containers and images in a single command.
+Show containers and images in a single command with special color highlighting
 #>
 
 if (!(Test-Elevated (Split-Path -Leaf $PSCommandPath) -warn)) { return }
 
+$e = [char]27
+
 Write-Host
 Write-Host 'docker ps -a' -ForegroundColor DarkYellow
-docker ps -a
-Write-Host
+
+$format = '{ ~id~: ~{{.ID}}~, ~names~: ~{{.Names}}~, ~image~: ~{{.Image}}~, ~status~: ~{{.Status}}~, ~ports~: ~{{.Ports}}~ }'
+$ps = ((docker ps -a --format $format --no-trunc) -join ',').Replace('~', '"')
+$ps = '{{ "containers": [ {0} ] }}' -f $ps | ConvertFrom-Json
+$ps.containers | % { $_.id = $_.id.Substring(0, 12) }
+
+$ps.containers | Format-Table id,
+	@{
+		Label = 'name'
+		Expression = {
+			if ($_.status -match '^Up') { $color = '92' } else { $color = '90' }
+			"$e[{0}m{1}$e[0m" -f $color,$_.names
+		}
+	},
+	@{
+		Label = 'image'
+		Expression = {
+			if ($_.image -match '^Waters') { $color = '94' } else { $color = '96' }
+			"$e[{0}m{1}$e[0m" -f $color,$_.image
+		}
+	},
+	status,ports
+
 Write-Host 'docker images' -ForegroundColor DarkYellow
-docker images
-Write-Host
+
+$format = '{ ~repository~: ~{{.Repository}}~, ~tag~: ~{{.Tag}}~, ~id~: ~{{.ID}}~, ~created~: ~{{.CreatedSince}}~, ~size~: ~{{.Size}}~ }'
+$im = ((docker images --format $format) -join ',').Replace('~', '"')
+$im = '{{ "images": [ {0} ] }}' -f $im | ConvertFrom-Json
+
+$im.images | Format-Table @{
+		Label='repository'
+		Expression = {
+			if ($_.repository -eq '<none>') { $color = '90' }
+			elseif ($_.repository -match '^Waters') { $color = '94' }
+			else { $color = '96' }
+			"$e[{0}m{1}$e[0m" -f $color,$_.repository
+		}
+	},
+	tag,id,created,size
