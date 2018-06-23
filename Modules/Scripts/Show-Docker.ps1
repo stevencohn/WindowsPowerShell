2ba95ed805538eaccc563566ec846a502bfd888e
Steven M. Cohn <steven_cohn@waters.com>
2018-06-23 09:51:33 -0400

    more formatting

--- Modules/Scripts/Show-Docker.ps1
+++ Modules/Scripts/Show-Docker.ps1
@@ -20,43 +20,59 @@ Write-Host 'docker ps -a' -ForegroundColor DarkYellow
 # Also the command will return an array of strings so first join them into a single
 # string, replace the tildes, and finally build the full Json string...
 
-$format = '{ ~id~: ~{{.ID}}~, ~names~: ~{{.Names}}~, ~image~: ~{{.Image}}~, ~status~: ~{{.Status}}~, ~ports~: ~{{.Ports}}~ }'
-$ps = ((docker ps -a --format $format --no-trunc) -join ',').Replace('~', '"')
-$ps = '{{ "containers": [ {0} ] }}' -f $ps | ConvertFrom-Json
+$ps = docker ps -a --no-trunc --format `
+	'{ ~id~: ~{{.ID}}~, ~names~: ~{{.Names}}~, ~image~: ~{{.Image}}~, ~status~: ~{{.Status}}~, ~ports~: ~{{.Ports}}~ }'
 
-# we used --no-trunc to get full command port strings but we do want to truncate ID
-$ps.containers | % { $_.id = $_.id.Substring(0, 12) }
+if ($ps)
+{
+	$ps = '{{ "containers": [ {0} ] }}' -f ($ps -join ',').Replace('~', '"') | ConvertFrom-Json
 
-$ps.containers | Format-Table id,
-	@{
-		Label = 'name'
-		Expression = {
-			if ($_.status -match '^Up') { $color = $Green } else { $color = $Gray }
-			"$e[{0}m{1}$e[0m" -f $color,$_.names
-		}
-	},
-	@{
-		Label = 'image'
-		Expression = {
-			if ($_.image -match '^Waters') { $color = $Blue } else { $color = $Cyan }
-			"$e[{0}m{1}$e[0m" -f $color,$_.image
-		}
-	},
-	status,ports
+	# we used --no-trunc to get full command port strings but we do want to truncate ID
+	$ps.containers | % { $_.id = $_.id.Substring(0, 12) }
+
+	$ps.containers | Format-Table id,
+		@{
+			Label = 'name'
+			Expression = {
+				if ($_.status -match '^Up') { $color = $Green } else { $color = $Gray }
+				"$e[{0}m{1}$e[0m" -f $color,$_.names
+			}
+		},
+		@{
+			Label = 'image'
+			Expression = {
+				if ($_.image -match '^Waters') { $color = $Blue } else { $color = $Cyan }
+				"$e[{0}m{1}$e[0m" -f $color,$_.image
+			}
+		},
+		status,ports
+}
+else
+{
+	Write-Host "No containers`n" -ForegroundColor DarkGray
+}
 
 Write-Host 'docker images' -ForegroundColor DarkYellow
 
-$format = '{ ~repository~: ~{{.Repository}}~, ~tag~: ~{{.Tag}}~, ~id~: ~{{.ID}}~, ~created~: ~{{.CreatedSince}}~, ~size~: ~{{.Size}}~ }'
-$im = ((docker images --format $format) -join ',').Replace('~', '"')
-$im = '{{ "images": [ {0} ] }}' -f $im | ConvertFrom-Json
+$im = docker images --format `
+	'{ ~id~: ~{{.ID}}~, ~repository~: ~{{.Repository}}~, ~tag~: ~{{.Tag}}~, ~created~: ~{{.CreatedSince}}~, ~size~: ~{{.Size}}~ }'
 
-$im.images | Format-Table @{
-		Label='repository'
-		Expression = {
-			if ($_.repository -eq '<none>') { $color = $Gray }
-			elseif ($_.repository -match '^Waters') { $color = $Blue }
-			else { $color = $Cyan }
-			"$e[{0}m{1}$e[0m" -f $color,$_.repository
-		}
-	},
-	tag,id,created,size
+if ($im)
+{
+	$im = '{{ "images": [ {0} ] }}' -f ($im -join ',').Replace('~', '"') | ConvertFrom-Json
+
+	$im.images | Format-Table id,@{
+			Label='repository'
+			Expression = {
+				if ($_.repository -eq '<none>') { $color = $Gray }
+				elseif ($_.repository -match '^Waters') { $color = $Blue }
+				else { $color = $Cyan }
+				"$e[{0}m{1}$e[0m" -f $color,$_.repository
+			}
+		},
+		tag,created,size
+}
+else
+{
+	Write-Host "No images`n" -ForegroundColor DarkGray
+}
