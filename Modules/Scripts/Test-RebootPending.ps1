7093710b913f6e0b236bbefc881696d7aee7e786
Steven M. Cohn <steven_cohn@waters.com>
2018-09-03 01:17:11 -0400

    Fix error if PendingFileRenameOpeartions doesn't exist

--- Modules/Scripts/Test-RebootPending.ps1
+++ Modules/Scripts/Test-RebootPending.ps1
@@ -49,8 +49,10 @@ Begin
 
 		# MSI file rename related
 		$0 = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager'
-		$msi = ((Get-ItemPropertyValue $0 -Name PendingFileRenameOperations -ErrorAction SilentlyContinue) | `
-			? { $_ -notmatch 'TEMP' -and $_ -ne '' }).Count -gt 0
+		$key = Get-Item -LiteralPath $0
+		$val = $key.GetValue('PendingFileRenameOperations', $null)
+		# ternary operation ?: indexed hashtable
+		$msi = @{$true=($val | ? { $_ -notmatch 'TEMP' -and $_ -ne '' }).Count -gt 0; $false=$false}[$null -ne $val]
 
 		## computer rename related
 		$currName = (Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' -Name 'ComputerName')
