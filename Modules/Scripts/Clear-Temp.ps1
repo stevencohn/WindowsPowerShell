46abb118f20fb3d542d3d1433a42107e18163a15
Steven Cohn <steven_cohn@waters.com>
2019-09-18 16:38:58 -0400

    add file/dir counts to reporting

--- Modules/Scripts/Clear-Temp.ps1
+++ Modules/Scripts/Clear-Temp.ps1
@@ -10,21 +10,67 @@ param([switch] $Quiet)
 
 
 $used = (Get-PSDrive C).Used
+$filCount = 0
+$dirCount = 0
 
-if (Test-Path 'C:\Temp')
+$0 = 'C:\Temp'
+if (Test-Path $0)
 {
-	Remove-Item -Path 'C:\Temp' -Force -Recurse -ErrorAction:SilentlyContinue
+	$fils = [System.IO.Directory]::GetFiles($0, '*').Count
+	$dirs = [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	Remove-Item -Path $0 -Force -Recurse -ErrorAction:SilentlyContinue
+
+	$fc = $fils - [System.IO.Directory]::GetFiles($0, '*').Count
+	$dc = $dirs - [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	$filCount += $fc
+	$dirCount += $dc
+
+	if (!$Quiet)
+	{
+		Write-Host "... removed $fc files, $dc directories from $0" -ForegroundColor DarkGray
+	}
 }
 
-if (Test-Path 'C:\Windows\Temp')
+$0 = 'C:\Windows\Temp'
+if (Test-Path $0)
 {
-	Remove-Item -Path 'C:\Windows\Temp' -Force -Recurse -ErrorAction:SilentlyContinue
+	$fils = [System.IO.Directory]::GetFiles($0, '*').Count
+	$dirs = [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	Remove-Item -Path $0 -Force -Recurse -ErrorAction:SilentlyContinue
+
+	$fc = $fils - [System.IO.Directory]::GetFiles($0, '*').Count
+	$dc = $dirs - [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	$filCount += $fc
+	$dirCount += $dc
+
+	if (!$Quiet)
+	{
+		Write-Host "... removed $fc files, $dc directories from $0" -ForegroundColor DarkGray
+	}
 }
 
-$t = Join-Path $env:LocalAppData 'Temp'
-if (Test-Path $t)
+$0 = Join-Path $env:LocalAppData 'Temp'
+if (Test-Path $0)
 {
-	Remove-Item -Path "$t\*" -Force -Recurse -ErrorAction:SilentlyContinue
+	$fils = [System.IO.Directory]::GetFiles($0, '*').Count
+	$dirs = [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	Remove-Item -Path "$0\*" -Force -Recurse -ErrorAction:SilentlyContinue
+
+	$fc = $fils - [System.IO.Directory]::GetFiles($0, '*').Count
+	$dc = $dirs - [System.IO.Directory]::GetDirectories($0, '*').Count
+
+	$filCount += $fc
+	$dirCount += $dc
+
+	if (!$Quiet)
+	{
+		Write-Host "... removed $fc files, $dc directories from $0" -ForegroundColor DarkGray
+	}
 }
 
 if (!$Quiet)
@@ -32,5 +78,6 @@ if (!$Quiet)
 	$disk = Get-PSDrive C | Select-Object Used,Free
 	$pct = ($disk.Used / ($disk.Used + $disk.Free)) * 100
 	$recovered = $used - $disk.Used
+	Write-Host "... removed $filCount files, $dirCount directories"
 	Write-Host ("... recovered {0:0.00} MB on drive C, {1:0.00}% used" -f ($recovered / 1024000), $pct)
 }
