1b757cc432a678f7cccc041a779625f32f042d6b
stevencohn <steven_cohn@waters.com>
2020-11-11 08:26:01 -0500

    Update-Gits should infer branch from config file

--- Modules/Scripts/Update-Gits.ps1
+++ Modules/Scripts/Update-Gits.ps1
@@ -4,7 +4,7 @@ Scan all sub-folders looking for Git repos and fetch/pull each of them to
 get latest code.
 
 .PARAMETER Branch
-Speciies the branch name, default is develop
+Speciies the branch name, default is read from the .git\config file
 
 .PARAMETER Project
 Specifies the project (subfolder) to update. If not specified then it will
@@ -17,7 +17,7 @@ for each repo before fetch and pull. This is to discard all local changes.
 
 param (
 	[Parameter(Position=0)] [string] $Project,
-	[Parameter(Position=1)] [string] $Branch = 'develop',
+	[Parameter(Position=1)] [string] $Branch,
 	[switch] $Reset
 )
 
@@ -38,15 +38,33 @@ Begin
 
         Push-Location $Project
 
+        $br = $branch
+        if (!$Branch)
+        {
+            $br = ReadBranch
+        }
+
         if ($Reset)
         {
-            git reset --hard origin/$Branch
+            git reset --hard origin/$br
         }
 
         git -c diff.mnemonicprefix=false -c core.quotepath=false fetch origin
-        git -c diff.mnemonicprefix=false -c core.quotepath=false pull --no-commit origin $Branch
+        git -c diff.mnemonicprefix=false -c core.quotepath=false pull --no-commit origin $br
 
         Pop-Location
+    }
+
+
+    function ReadBranch
+    {
+        $a = Get-Content .\.git\config | Select-String -Pattern '^\[branch "(.+)"\]$'
+        if ($a.Matches.Success)
+        {
+            return $a.Matches.Groups[1].Value
+        }
+
+        return 'master'
     }
 }
 Process
