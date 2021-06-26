747b9edfc257927bcca368823258989fafb14f67
Steven Cohn <stevencohn@live.com>
2021-06-26 08:37:08 -0400

    notifyicon

--- Modules/Scripts/Set-NotifyIcon.ps1
+++ Modules/Scripts/Set-NotifyIcon.ps1
@@ -5,100 +5,86 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
 
 param(
-    [Parameter(Mandatory = $true, HelpMessage = 'The name of the program')][string]$ProgramName,
-    [Parameter(Mandatory = $true, HelpMessage =
-        'The setting (2 = show icon and notifications 1 = hide icon and notifications, 0 = only show notifications')]
-    [ValidateScript( { if ($_ -lt 0 -or $_ -gt 2) { throw 'Invalid setting' } return $true })]
-    [Int16]$Setting
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage='Path of program')]
+        [string] $ProgramPath,
+    [Parameter(
+        HelpMessage = '0=only show notifications, 1=hide, 2=show icon and notifications')]
+        [ValidateScript( { if ($_ -lt 0 -or $_ -gt 2) { throw 'Invalid setting' } return $true })]
+        [Int16] $Setting = 2
 )
 
 Begin
 {
-    function Rot13($byteToRot)
+    $script:TrayKey = 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify'
+
+    function GetStreamData
     {
-        if ($byteToRot -gt 64 -and $byteToRot -lt 91)
-        {
-            $bytRot = $($($byteToRot - 64 + 13) % 26 + 64)
-            return $bytRot
-        }
-        elseif ($byteToRot -gt 96 -and $byteToRot -lt 123)
-        {
-            $bytRot = $($($byteToRot - 96 + 13) % 26 + 96)
-            return $bytRot
-        }
-        else
-        {
-            return $byteToRot
-        }
+        param([byte[]] $streams)
+        $builder = New-Object System.Text.StringBuilder
+        
+        # this line will ROT13 the data so you view/debug the ASCII contents of the stream
+        #$streams | % { if ($_ -ge 32 -and $_ -le 125) { [void]$builder.Append( [char](Rot13 $_) ) } };
+
+        $streams | % { [void]$builder.Append( ('{0:x2}' -f $_) ) }
+        return $builder.ToString()
+    }
+
+    function EncodeProgramPath
+    {
+        param([string] $path)
+
+        $encoding = New-Object System.Text.UTF8Encoding
+        $bytes = $encoding.GetBytes($path)
+
+        $builder = New-Object System.Text.StringBuilder
+        $bytes | % { [void]$builder.Append( ('{0:x2}00' -f (Rot13 $_)) ) }
+        return $builder.ToString()
+    }
+
+    function Rot13
+    {
+        param([byte] $byte)
+
+            if ($byte -ge  65 -and $byte -le  77) { return $byte + 13 } # A..M
+        elseif ($byte -ge  78 -and $byte -le  90) { return $byte - 13 } # N..Z
+        elseif ($byte -ge  97 -and $byte -le 109) { return $byte + 13 } # a..m
+        elseif ($byte -ge 110 -and $byte -le 122) { return $byte - 13 } # n..z
+        
+        return $byte
     }
 }
 Process
 {
-    $encText = New-Object System.Text.UTF8Encoding
-    [byte[]] $bytRegKey = @()
-    $strRegKey = ""
+    $streams = (Get-ItemProperty (Get-Item $TrayKey).PSPath).IconStreams
 
-    $0 = 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify'
-    $bytRegKey = $(Get-ItemProperty $(Get-Item $0).PSPath).IconStreams
+    $data = GetStreamData $streams
 
-    for ($x = 0; $x -le $bytRegKey.Count; $x++)
+    $path = EncodeProgramPath $ProgramPath
+    #Write-Host ( $path.Split('00') | ? { $_.Length -gt 0 } | % { [char](Rot13 ([Convert]::ToByte($_, 16))) } )
+
+    if (-not $data.Contains($path))
     {
-        $tempString = [Convert]::ToString($bytRegKey[$x], 16)
-        switch ($tempString.Length)
-        {
-            0 { $strRegKey += "00" }
-            1 { $strRegKey += "0" + $tempString }
-            2 { $strRegKey += $tempString }
-        }
+        Write-Warning "$ProgramPath not found. Programs are case sensitive."
+        return
     }
 
-    [byte[]] $bytTempAppPath = @()
-    $bytTempAppPath = $encText.GetBytes($ProgramName)
-    [byte[]] $bytAppPath = @()
-    $strAppPath = ""
-
-    for ($x = 0; $x -lt $bytTempAppPath.Count * 2; $x++)
-    {
-        If ($x % 2 -eq 0)
-        {
-            $curbyte = $bytTempAppPath[$([Int]($x / 2))]
-            $bytAppPath += Rot13($curbyte)
-        }
-        Else
-        {
-            $bytAppPath += 0
-        }
-    }
-
-    for ($x = 0; $x -lt $bytAppPath.Count; $x++)
-    {
-        $tempString = [Convert]::ToString($bytAppPath[$x], 16)
-        switch ($tempString.Length)
-        {
-            0 { $strAppPath += "00" }
-            1 { $strAppPath += "0" + $tempString }
-            2 { $strAppPath += $tempString }
-        }
-    }
-
-    if (-not $strRegKey.Contains($strAppPath))
-    {
-        Write-Host Program not found. Programs are case sensitive.
-        break
-    }
+    write-host 'Found!'
+    return
 
     [byte[]] $header = @()
     $items = @{}
     for ($x = 0; $x -lt 20; $x++)
     {
-        $header += $bytRegKey[$x]
+        $header += $streams[$x]
     }
 
-    for ($x = 0; $x -lt $(($bytRegKey.Count - 20) / 1640); $x++)
+    for ($x = 0; $x -lt $(($streams.Count - 20) / 1640); $x++)
     {
         [byte[]] $item = @()
         $startingByte = 20 + ($x * 1640)
-        $item += $bytRegKey[$($startingByte)..$($startingByte + 1639)]
+        $item += $streams[$($startingByte)..$($startingByte + 1639)]
         $items.Add($startingByte.ToString(), $item)
     }
 
@@ -119,17 +105,18 @@ Process
         }
         if ($strItem.Contains($strAppPath))
         {
-            Write-Host Item Found with $ProgramName in item starting with byte $key
-            $bytRegKey[$([Convert]::ToInt32($key) + 528)] = $setting
+            Write-Host Item Found with $ProgramPath in item starting with byte $key
+            $streams[$([Convert]::ToInt32($key) + 528)] = $setting
+
+            $0 = (Get-Item $TrayKey).PSPath
 
             if (!$WhatIfPreference)
             {
-                $0 = 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify'
-                Set-ItemProperty $($(Get-Item $0).PSPath) -name IconStreams -value $bytRegKey
+                Set-ItemProperty $0 -name IconStreams -value $streams
             }
             else
             {
-                Write-Host Foo
+                Write-Host "Set-ItemProperty '$0' -name IconStreams -value $streams"
             }
         }
     }
