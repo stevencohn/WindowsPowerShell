c75d80fb164a72ac5103d2af70321cd0623bfa0b
Steven Cohn <stevencohn@live.com>
2022-02-22 13:10:36 -0500

    no

--- Install-VS.ps1
+++ Install-VS.ps1
@@ -105,12 +105,7 @@ Begin
 		InstallVsix $installer 'VSColorOutput'
 
 		Write-Host
-		Write-Host '... waiting for all VSIXInstaller processes to complete; this will take a couple of minutes' -Fore Yellow
-		Write-Host '... ' -NoNewline
-
-		Wait-Process -Name 'VSIXInstaller'
-
-		Write-host '... installation complete'
+		Write-Host '... Wait a couple of minutes for the VSIXInstaller processes to complete before starting VS' -Fore Yellow
 	}
 
 
