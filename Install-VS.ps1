e546b59925f5f18efae8c162014251d29d4795e9
Steven Cohn <stevencohn@live.com>
2024-02-17 08:02:47 -0500

    VSTextMacros

--- Install-VS.ps1
+++ Install-VS.ps1
@@ -136,8 +136,8 @@ Begin
 		InstallVsix $installer 'MarkdownEditor' 	# Github
 		InstallVsix $installer 'SonarLint' 			# SonarSource.SonarLintforVisualStudio2022
 		InstallVsix $installer 'SpecFlow'			# TechTalkSpecFlowTeam.SpecFlowForVisualStudio2022
-		InstallVsix $installer 'TextMacrosforVisualStudio201220132015'	# XavierPoinas.TextMacrosforVisualStudio201220132015
 		InstallVsix $installer 'VSColorOutput'		# MikeWard-AnnArbor.VSColorOutput64
+		InstallVsix $installer 'VSTextMacros-1.18'	# XavierPoinas.TextMacrosforVisualStudio201220132015
 
 		Write-Host
 		WriteWarn '... Wait a couple of minutes for the VSIXInstaller processes to complete before starting VS'
