b74a312d793cee2262c9e5a60ca5e4159c4789a1
Steven Cohn <stevencohn@live.com>
2024-08-12 07:12:36 -0400

    notes

--- Modules/Scripts/Invoke-NormalUser.ps1
+++ Modules/Scripts/Invoke-NormalUser.ps1
@@ -12,5 +12,15 @@ param (
 	[string] $Command
 	)
 
-#https://superuser.com/questions/1749696/parameter-is-incorrect-when-using-runas-with-trustlevel-after-windows-11-22h2
+<#
+https://superuser.com/questions/1749696/parameter-is-incorrect-when-using-runas-with-trustlevel-after-windows-11-22h2
+
+The workaround with /machine:x86|amd64|arm|arm64 works. If you are on a 64bit intel machine and
+want to execute a batchfile there is no machine option that will work since cmd.exe is a 64 bit
+application. There is a 32bit version for cmd.exe at C:\Windows\sysWOW64\cmd.exe, so a command
+could be as follows:
+
+runas /machine:x86 /trustlevel:0x20000 "C:\Windows\sysWOW64\cmd.exe <yourBatchfileHere>"
+#>
+
 runas /machine:x86 /trustlevel:0x20000 "C:\Windows\sysWOW64\cmd.exe /c `"$Command`""
